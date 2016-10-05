f3cbeee01e80bd4b8ba14fb24be111096ccc4961
Brit Butler <redline6561@gmail.com>
2016-10-05 09:55:22 -0400

    A little bottom-up sketch of the basic calculator.

---
+++ normal.js
@@ -0,0 +1,8 @@
+var sumInputs = function () {
+  return sum;
+};
+
+var getAnswer = function () { };
+
+var button = document.getElementById("calculate-button");
+button.addEventListener("click", getAnswer);
